195c4e5a3c2221388cd5bab764e7c4194e546ce7
jyoti1404 <juhipanjwani04@gmail.com>
2020-06-09 14:33:26 +0530

    done

--- app/src/main/java/com/example/purpledocs_assign/Main2Activity.java
+++ app/src/main/java/com/example/purpledocs_assign/Main2Activity.java
@@ -2,13 +2,161 @@ package com.example.purpledocs_assign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    Dialog dialog1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Boolean isFirstRun2 = getSharedPreferences("PREFERENCE2", MODE_PRIVATE)
+                .getBoolean("isFirstRun2", true);
+
+        DisplayMetrics displayMetrics1 = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics1);
+
+        float yInches= displayMetrics1.heightPixels/displayMetrics1.ydpi;
+        float xInches= displayMetrics1.widthPixels/displayMetrics1.xdpi;
+
+        double diagonalInches1 = Math.sqrt(xInches*xInches + yInches*yInches);
+
+        Configuration config1 = getResources().getConfiguration();
+
+
+        if (config1.smallestScreenWidthDp >= 600){
+            if (isFirstRun2){
+                dialog1 = new Dialog(Main2Activity.this, R.style.Theme_AppCompat_Translucent);
+                dialog1.setContentView(R.layout.overlay3_tab);
+
+                ImageView arrow4 = dialog1.findViewById(R.id.arrow4);
+                arrow4.setImageResource(R.drawable.arrow5_removebg);
+                Animation animation4 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow4.startAnimation(animation4);
+
+                TextView caseno = dialog1.findViewById(R.id.caseno);
+                caseno.setText("Type the number \n of patient to be \n searched");
+
+                ImageView arrow5 = dialog1.findViewById(R.id.arrow5);
+                arrow5.setImageResource(R.drawable.arrow6_removebg);
+                Animation animation5 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow5.startAnimation(animation5);
+
+                TextView name = dialog1.findViewById(R.id.name);
+                name.setText("Type the name \n of patient to be \n searched");
+
+                ImageView arrow6 = dialog1.findViewById(R.id.arrow6);
+                arrow6.setImageResource(R.drawable.arrow7_removebg);
+                Animation animation6 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow6.startAnimation(animation6);
+
+                TextView contact = dialog1.findViewById(R.id.contact);
+                contact.setText("Type the contact \n no of patient \n (upto 15 digits)");
+
+                ImageView arrow7 = dialog1.findViewById(R.id.arrow7);
+                arrow7.setImageResource(R.drawable.arrow8_removebg);
+                Animation animation7 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow7.startAnimation(animation7);
+
+                TextView email = dialog1.findViewById(R.id.email);
+                email.setText("Type the email \n of patient to be \n searched");
+
+                ImageView arrow8 = dialog1.findViewById(R.id.arrow8);
+                arrow8.setImageResource(R.drawable.arrow9_removebg);
+                Animation animation8 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow8.startAnimation(animation8);
+
+                TextView search = dialog1.findViewById(R.id.search);
+                search.setText("Tap here to \n search the patient");
+
+                dialog1.setCancelable(true);
+                dialog1.show();
+
+            }
+            getSharedPreferences("PREFERENCE2", MODE_PRIVATE).edit().putBoolean("isFirstRun2", false).commit();
+
+        }else {
+            if (isFirstRun2){
+                dialog1 = new Dialog(Main2Activity.this, R.style.Theme_AppCompat_Translucent);
+                dialog1.setContentView(R.layout.overlay3);
+
+                ImageView arrow4 = dialog1.findViewById(R.id.arrow4);
+                arrow4.setImageResource(R.drawable.arrow5_removebg);
+                Animation animation4 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow4.startAnimation(animation4);
+
+                TextView caseno = dialog1.findViewById(R.id.caseno);
+                caseno.setText("Type the number \n of patient to be \n searched");
+
+                ImageView arrow5 = dialog1.findViewById(R.id.arrow5);
+                arrow5.setImageResource(R.drawable.arrow6_removebg);
+                Animation animation5 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow5.startAnimation(animation5);
+
+                TextView name = dialog1.findViewById(R.id.name);
+                name.setText("Type the name \n of patient to be \n searched");
+
+                ImageView arrow6 = dialog1.findViewById(R.id.arrow6);
+                arrow6.setImageResource(R.drawable.arrow7_removebg);
+                Animation animation6 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow6.startAnimation(animation6);
+
+                TextView contact = dialog1.findViewById(R.id.contact);
+                contact.setText("Type the contact \n no of patient \n (upto 15 digits)");
+
+                ImageView arrow7 = dialog1.findViewById(R.id.arrow7);
+                arrow7.setImageResource(R.drawable.arrow8_removebg);
+                Animation animation7 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow7.startAnimation(animation7);
+
+                TextView email = dialog1.findViewById(R.id.email);
+                email.setText("Type the email \n of patient to be \n searched");
+
+                ImageView arrow8 = dialog1.findViewById(R.id.arrow8);
+                arrow8.setImageResource(R.drawable.arrow9_removebg);
+                Animation animation8 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow8.startAnimation(animation8);
+
+                TextView search = dialog1.findViewById(R.id.search);
+                search.setText("Tap here to \n search the patient");
+
+                dialog1.setCancelable(true);
+                dialog1.show();
+
+            }
+
+            getSharedPreferences("PREFERENCE2", MODE_PRIVATE).edit().putBoolean("isFirstRun2", false).commit();
+        }
+
     }
 }
--- app/src/main/java/com/example/purpledocs_assign/MainActivity.java
+++ app/src/main/java/com/example/purpledocs_assign/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.purpledocs_assign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.ViewGroup;
@@ -22,76 +23,83 @@ public class MainActivity extends AppCompatActivity {
 
         Boolean isFirstRun = getSharedPreferences("PREFERENCE1", MODE_PRIVATE)
                 .getBoolean("isFirstRun1", true);
+//
+//        DisplayMetrics metrics = new DisplayMetrics();
+//        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+//
+//        float yInches= metrics.heightPixels/metrics.ydpi;
+//        float xInches= metrics.widthPixels/metrics.xdpi;
+//
+//        double diagonalInches = Math.sqrt(xInches*xInches + yInches*yInches);
 
-        DisplayMetrics metrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        Configuration config = getResources().getConfiguration();
 
-        float yInches= metrics.heightPixels/metrics.ydpi;
-        float xInches= metrics.widthPixels/metrics.xdpi;
+        if (config.smallestScreenWidthDp >= 600){
 
-        double diagonalInches = Math.sqrt(xInches*xInches + yInches*yInches);
+            if (isFirstRun) {
 
-        if (diagonalInches >= 6.5){
+                // 6.5inch device or bigger
+                dialog = new Dialog(MainActivity.this, R.style.Theme_AppCompat_Translucent);
+                dialog.setContentView(R.layout.activity_overlay2_tab);
 
-            // 6.5inch device or bigger
-            dialog = new Dialog(MainActivity.this, R.style.Theme_AppCompat_Translucent);
-            dialog.setContentView(R.layout.activity_overlay2);
+                TextView appointment = dialog.findViewById(R.id.appointment);
+                appointment.setText("Tap to Book \n your Appointment");
+                appointment.setTextSize(18);
 
-            TextView appointment = dialog.findViewById(R.id.appointment);
-            appointment.setText("Tap to Book \n your Appointment");
-            appointment.setTextSize(18);
+//                float text1size = getResources().getDimension(R.dimen.tab_text1_margin_start);
+//                appointment.setPadding((int) text1size, 0, 0, 0);
 
-            float text1size = getResources().getDimension(R.dimen.tab_text1_margin_start);
-            appointment.setPadding((int) text1size, 0, 0, 0);
+                TextView notification = dialog.findViewById(R.id.notification);
+                notification.setText("Tap to see \n new notifications");
+                notification.setTextSize(18);
 
+                ImageView arrow1 = dialog.findViewById(R.id.arrow1);
+                arrow1.setImageResource(R.drawable.arrow1_removebg);
+//                float arrow1size = getResources().getDimension(R.dimen.tab_arrow1_margin_start);
+//                appointment.setPadding((int) arrow1size, 0, 0, 0);
 
-            TextView notification = dialog.findViewById(R.id.notification);
-            notification.setText("Tap to see \n new notifications");
-            notification.setTextSize(18);
-
-            ImageView arrow1 = dialog.findViewById(R.id.arrow1);
-            arrow1.setImageResource(R.drawable.arrow1_removebg);
-            float arrow1size = getResources().getDimension(R.dimen.tab_arrow1_margin_start);
-            appointment.setPadding((int) arrow1size, 0, 0, 0);
-
-            Animation animation1 =
-                    AnimationUtils.loadAnimation(getApplicationContext(),
-                            R.anim.blink);
-            arrow1.startAnimation(animation1);
+                Animation animation1 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow1.startAnimation(animation1);
 
 //            AnimationDrawable frameAnimation = (AnimationDrawable) arrow1.getBackground();
 //            // Start the animation (looped playback by default).
 //            frameAnimation.start();
 //
-            TextView add = dialog.findViewById(R.id.add);
-            add.setText("Tap to add \n a patient");
-            add.setTextSize(18);
+                TextView add = dialog.findViewById(R.id.add);
+                add.setText("Tap to add \n a patient");
+                add.setTextSize(18);
 
-            TextView queue = dialog.findViewById(R.id.queue);
-            queue.setText("Patient Queue");
-            queue.setTextSize(30);
+                TextView queue = dialog.findViewById(R.id.queue);
+                queue.setText("Patient Queue");
+                queue.setTextSize(30);
 
-            ImageView arrow2 = dialog.findViewById(R.id.arrow2);
-            arrow2.setImageResource(R.drawable.arrow2_removebg);
-            float arrow2size = getResources().getDimension(R.dimen.tab_arrow2_margin_start);
-            arrow2.setPadding((int) arrow2size, 0, 0, 0);
-            Animation animation3 =
-                    AnimationUtils.loadAnimation(getApplicationContext(),
-                            R.anim.blink);
-            arrow2.startAnimation(animation3);
+                ImageView arrow2 = dialog.findViewById(R.id.arrow2);
+                arrow2.setImageResource(R.drawable.arrow2_removebg);
+//                float arrow2size = getResources().getDimension(R.dimen.tab_arrow2_margin_start);
+//                arrow2.setPadding((int) arrow2size, 0, 0, 0);
+                Animation animation3 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow2.startAnimation(animation3);
 
-            ImageView arrow3 = dialog.findViewById(R.id.arrow3);
-            arrow3.setImageResource(R.drawable.arrow3_removebg);
-            float arrow3size = getResources().getDimension(R.dimen.tab_arrow1_margin_start);
-            appointment.setPadding((int) arrow3size, 0, 0, 0);
+                ImageView arrow3 = dialog.findViewById(R.id.arrow3);
+                arrow3.setImageResource(R.drawable.arrow3_removebg);
+//                float arrow3size = getResources().getDimension(R.dimen.tab_arrow1_margin_start);
+//                appointment.setPadding((int) arrow3size, 0, 0, 0);
 
-            Animation animation2 =
-                    AnimationUtils.loadAnimation(getApplicationContext(),
-                            R.anim.blink);
-            arrow3.startAnimation(animation2);
+                Animation animation2 =
+                        AnimationUtils.loadAnimation(getApplicationContext(),
+                                R.anim.blink);
+                arrow3.startAnimation(animation2);
 
-            dialog.setCancelable(true);
-            dialog.show();
+                dialog.setCancelable(true);
+                dialog.show();
+
+
+            }
+            getSharedPreferences("PREFERENCE1", MODE_PRIVATE).edit().putBoolean("isFirstRun1", true);
 
         }
         else{
@@ -156,8 +164,8 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
 
             }
-
             getSharedPreferences("PREFERENCE1", MODE_PRIVATE).edit().putBoolean("isFirstRun1", false).commit();
+
         }
 
     }
