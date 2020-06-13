a3131a50371ecba1046c90b0f2920b23d4c63afa
jyoti1404 <juhipanjwani04@gmail.com>
2020-06-13 16:28:20 +0530

    done

--- app/src/main/java/com/example/purpledocs_assign/AddPatientViewPager.java
+++ app/src/main/java/com/example/purpledocs_assign/AddPatientViewPager.java
@@ -5,12 +5,14 @@ import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.ViewFlipper;
 
 public class AddPatientViewPager extends AppCompatActivity {
 
 //    ViewPager viewPager;
     ViewFlipper viewFlipper;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +23,26 @@ public class AddPatientViewPager extends AppCompatActivity {
 //        viewPager.setAdapter(new CustomPageAdapter(this));
 
         viewFlipper = findViewById(R.id.viewFlipper);
+        textView = findViewById(R.id.main_textView);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE1", MODE_PRIVATE)
+                .getBoolean("isFirstRun1", true);
 
-        int image[]={R.drawable.view1,R.drawable.view2,R.drawable.view3,R.drawable.view4};
-        for(int slide:image)
-        {
-            flipperImages(slide);
+        int image[]={R.drawable.view1,R.drawable.view2,R.drawable.view3,R.drawable.view4, R.drawable.view5, R.drawable.view6};
+
+        if (isFirstRun) {
+            for (int slide : image) {
+                flipperImages(slide);
+            }
+            textView.setText("");
         }
+        getSharedPreferences("PREFERENCE1", MODE_PRIVATE).edit().putBoolean("isFirstRun1", false).commit();
     }
     public void flipperImages(int image)
     {
         ImageView imageView=new ImageView(this);
         imageView.setBackgroundResource(image);
         viewFlipper.addView(imageView);
-        viewFlipper.setFlipInterval(3000);
+        viewFlipper.setFlipInterval(6000);
         viewFlipper.setAutoStart(true);
         viewFlipper.setInAnimation(this,android.R.anim.fade_in);
         viewFlipper.setOutAnimation(this,android.R.anim.fade_out);
